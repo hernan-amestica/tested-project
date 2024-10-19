fc5cc8e3dfcf7f085c4d2a081f80d59bfc49a476
hernan-amestica <hernan.amestica.r@gmail.com>
2024-10-19 18:17:00 -0300

    index test update

--- src/app/app.component.spec.ts
+++ src/app/app.component.spec.ts
@@ -17,7 +17,7 @@ describe('AppComponent', () => {
   it(`should have the 'tested-project' title`, () => {
     const fixture = TestBed.createComponent(AppComponent);
     const app = fixture.componentInstance;
-    expect(app.title).toEqual('tested-project');
+    expect(app.title).toEqual('tested-projectt');
   });
 
   it('should render title', () => {
